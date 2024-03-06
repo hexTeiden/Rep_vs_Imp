c27cf01ae407766597fe815ebaeff070251bb092
Teiden <knogler.m19@htlwienwest.at>
2024-03-06 21:37:44 +0100

    Add new routes for Contact and About pages

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,6 +12,16 @@ const routes = [
     path: '/imperium,',
     children: [{ path: '', component: () => import('pages/ImperiumPage.vue') }],
   },
+  {
+    path: '/contact',
+    component: () => import('pages/ContactLayout.vue'),
+    children: [{ path: '', component: () => import('pages/ContactPage.vue') }],
+  },
+  {
+    path: '/about',
+    component: () => import('pages/AboutLayout.vue'),
+    children: [{ path: '', component: () => import('pages/AboutPage.vue') }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it

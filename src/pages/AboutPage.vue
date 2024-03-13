4ab06e3a0c6c0fadff4579e09fb1f1de46328847
Hex Teiden <knogler.m19@htlwienwest.at>
2024-03-13 12:27:16 +0100

    I legit forgot how much shit I just did there, all I know is I remade the entire page and finished it

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,17 +1,67 @@
 <template>
-  <div class="flex flex-center">
-    <q-card>
-      <q-card-section>
-        <h2>Über das Projekt <br />"Imperium vs Republik"</h2>
-        <p>
-          Der Leitgedanke unseres Teams war es Menschen den Unterschied der politischen Führungsform
-          <br />der galaktischen Republik und des galaktischen Imperiums leichter zu erklären. Viele
-          Menschen <br />(vor allem Menschen die Star Wars nicht gesehen haben) wissen nicht was der
-          Unterschied zwischen <br />
-          der Republik und des Imperiums ist und dementsprechend auch nicht wie sie über die Galaxie
-          herrschen.
-        </p>
+  <div class="q-pa-md q-ma-lg row items-start q-gutter-md">
+    <q-card class="inset-shadow-down" flat bordered>
+      <q-card-section horizontal>
+        <q-card-section class="q-pt-xs">
+          <div class="text-h4 q-mt-sm q-mb-xs">Über das Projekt "Imperium vs Republik"</div>
+          <div class="text-caption">
+            Der Leitgedanke unseres Teams war es Menschen den Unterschied der politischen
+            Führungsform der galaktischen Republik und des galaktischen Imperiums leichter zu
+            erklären. Viele Menschen (vor allem Menschen die Star Wars nicht gesehen haben) wissen
+            nicht was der Unterschied zwischen der Republik und des Imperiums ist und
+            dementsprechend auch nicht wie sie über die Galaxie herrschen.
+            <p class="text-overline">Projektteam</p>
+            <span>
+              <q-img
+                src="../images/Test.jpg"
+                :ratio="16 / 9"
+                spinner-color="primary"
+                spinner-size="82px"
+                class="img"
+                id="Knogler"
+                @click="KnoglerPopup()"
+              />
+              <div class="popup">
+                <span class="popuptext" id="popupknogler"> Matteo Knogler </span>
+              </div>
+            </span>
+            <span>
+              <q-img
+                src="../images/Test.jpg"
+                :ratio="16 / 9"
+                spinner-color="primary"
+                spinner-size="82px"
+                class="img"
+                id="Iveljic"
+                @click="IveljicPopup()"
+              />
+              <div class="popup">
+                <span class="popuptext" id="popupivejic"> Lukas Iveljic </span>
+              </div>
+            </span>
+            <span>
+              <q-img
+                src="../images/Test.jpg"
+                :ratio="16 / 9"
+                spinner-color="primary"
+                spinner-size="82px"
+                class="img"
+                id="Sadat"
+                @click="SadatPopup()"
+              />
+              <div class="popup">
+                <span class="popuptext" id="popupsadat"> Sayed Sadat </span>
+              </div>
+            </span>
+          </div>
+        </q-card-section>
+
+        <q-card-section class="col-5 flex flex-center">
+          <q-img class="rounded-borders" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+        </q-card-section>
       </q-card-section>
+
+      <q-separator />
     </q-card>
   </div>
 </template>
@@ -21,5 +71,80 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'AboutPage',
+
+  methods: {
+    KnoglerPopup() {
+      let popup = document.querySelector('#popupknogler');
+      popup.classList.toggle('show');
+    },
+    IveljicPopup() {
+      let popup = document.querySelector('#popupivejic');
+      popup.classList.toggle('show');
+    },
+    SadatPopup() {
+      let popup = document.querySelector('#popupsadat');
+      popup.classList.toggle('show');
+    },
+  },
 });
 </script>
+
+<style lang="sass">
+.img
+  size: 100%
+  margin-top: 20px
+  margin-left: 20px
+  margin-right: 20px
+  margin-bottom: 20px
+  border-radius: 10px
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5)
+  width: 200px
+  height: 200px
+
+  /* Popup container */
+.popup
+  position: relative
+  display: inline-block
+  cursor: pointer
+
+
+/* The actual popup (appears on top) */
+.popup .popuptext
+  visibility: hidden
+  width: 160px
+  background-color: #555
+  color: #fff
+  text-align: center
+  border-radius: 6px
+  padding: 8px 0
+  position: absolute
+  z-index: 1
+  bottom: 125%
+  left: 50%
+  margin-left: -80px
+
+
+/* Toggle this class when clicking on the popup container (hide and show the popup) */
+.popup .show
+  visibility: visible
+  -webkit-animation: fadeIn 1s
+  animation: fadeIn 1s
+
+
+/* Add animation (fade in the popup) */
+@-webkit-keyframes fadeIn
+  from
+    opacity: 0
+
+  to
+    opacity: 1
+
+
+
+@keyframes fadeIn
+  from
+    opacity: 0
+
+  to
+    opacity: 1
+</style>

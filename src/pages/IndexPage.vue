c3dc8134e3a2e0fbf5531b68c13711246be9a6bf
Teiden <knogler.m19@htlwienwest.at>
2024-03-13 22:40:18 +0100

    Add border-radius to UserCards.vue and embed Juxtapose in IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="flex flex-center">
+    <iframe
+      frameborder="0"
+      class="juxtapose"
+      width="100%"
+      height="600"
+      src="https://cdn.knightlab.com/libs/juxtapose/latest/embed/index.html?uid=b28f78c0-e180-11ee-9685-5d0fb8b12f54"
+    ></iframe>
+  </q-page>
 </template>
 
 <script>
@@ -9,3 +17,5 @@ export default defineComponent({
   name: 'IndexPage',
 });
 </script>
+
+<style lang="sass"></style>
